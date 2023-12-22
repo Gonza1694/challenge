bed64bc8d2c976f9417c9bd5862ae4b3700b6b9d
Gon <gonzaa1694@gmail.com>
2023-12-22 15:52:52 -0300

    update program adding services

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -1,3 +1,5 @@
+using WeatherAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<IGeocodingService, GeocodingService>(provider =>
+{
+    var apiKey = "56025b0ebe4fbe40a1e325304e2669ef";
+    return new GeocodingService(provider.GetRequiredService<IHttpClientFactory>(), apiKey);
+});
+
+builder.Services.AddScoped<IWeatherService, WeatherService>(provider =>
+{
+    var apiKey = "56025b0ebe4fbe40a1e325304e2669ef";
+    return new WeatherService(provider.GetRequiredService<IHttpClientFactory>(), apiKey);
+});
 
 var app = builder.Build();
 
