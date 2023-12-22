d5caa4063be4c5daaf79e6af16632c5a2c9c6d9d
Gon <gonzaa1694@gmail.com>
2023-12-22 15:45:38 -0300

    add geocoding controller

---
+++ WeatherAPI/Controllers/GeocodingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class GeocodingController : ControllerBase
+{
+    private readonly IGeocodingService _geocodingService;
+
+    public GeocodingController(IGeocodingService geocodingService)
+    {
+        _geocodingService = geocodingService;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<ActionResult<object>> Get()
+    {
+        var city = "Buenos Aires";
+
+        try
+        {
+            var cityCoordinates = await _geocodingService.GetCityCoordinates(city);
+            return Ok(cityCoordinates);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+}
