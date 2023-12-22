a8d72d210a63c0d8d3e848441292b8445824079e
Gon <gonzaa1694@gmail.com>
2023-12-22 15:49:55 -0300

    add weather controller -api

---
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private readonly IGeocodingService _geocodingService;
+    private readonly IWeatherService _weatherService;
+
+    public WeatherController(IGeocodingService geocodingService, IWeatherService weatherService)
+    {
+        _geocodingService = geocodingService;
+        _weatherService = weatherService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetWeatherByCityAsync()
+    {
+        var city = "Buenos Aires";
+
+        try
+        {
+            var cityCoordinates = await _geocodingService.GetCityCoordinates(city);
+
+            var weatherData = await _weatherService.GetWeatherByCoordinatesAsync(cityCoordinates.Lat, cityCoordinates.Lon);
+
+            return Ok(weatherData);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error: {ex.Message}");
+        }
+    }
+}
