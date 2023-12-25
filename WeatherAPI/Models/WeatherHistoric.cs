cc5b9dcc3279f923e139ff9461b56793d66430c8
Gon <gonzaa1694@gmail.com>
2023-12-25 17:52:56 -0300

    add historic model - api

---
+++ WeatherAPI/Models/WeatherHistoric.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherAPI.Models
+{
+    public class WeatherHistoric
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public double Temperature { get; set; }
+        public double Feels_Like { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
