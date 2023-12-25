98fd9ea37f6b24edc4927b5d5d7b0baabbba9cfa
Gon <gonzaa1694@gmail.com>
2023-12-25 17:52:40 -0300

    add db context - api

---
+++ WeatherAPI/Data/WeatherDbContext .cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAPI.Data.Repositories;
+using WeatherAPI.Models;
+
+namespace WeatherAPI.Data
+{
+    public class WeatherDbContext : DbContext
+    {
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<WeatherHistoric> WeatherHistorics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
