821345ae56ed41eed3923473936bce130509de03
Gon <gonzaa1694@gmail.com>
2023-12-25 17:51:50 -0300

    add weatherResponse model - api

---
+++ WeatherAPI/Models/WeatherResponse.cs
@@ -0,0 +1,20 @@
+﻿using WeatherAPI.Services;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherResponse
+    {
+        public Main Main { get; set; }
+        public Sys Sys { get; set; }
+        public string Name { get; set; }
+    }
+    public class Sys
+    {
+        public string Country { get; set; }
+    }
+    public class Main
+    {
+        public double Temp { get; set; }
+        public double Feels_Like { get; set; }
+    }
+}
