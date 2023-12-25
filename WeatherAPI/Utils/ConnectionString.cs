f20986b0dfe88f3fa9c07813770b6fd0b5fb0789
Gon <gonzaa1694@gmail.com>
2023-12-25 17:52:27 -0300

    add connection string - api

---
+++ WeatherAPI/Utils/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿namespace WeatherAPI.Utils
+{
+    public class ConnectionString
+    {
+        private const string Server = @"GON\WEATHER";
+
+        private const string DB = @"WeatherDB";
+        private const string User = @"sa";
+        private const string Password = @"1398632";
+
+        public static string GetSQLString => $"Data Source={Server};" +
+                                                 $"Initial Catalog={DB}; " +
+                                                 $"User Id={User}; " +
+                                                 $"Password={Password};" +
+                                                 $"TrustServerCertificate=true;";
+    }
+}
