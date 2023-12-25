46b39638ad8c034bc12ebb8ef0d9b7b419d7ac2b
Gon <gonzaa1694@gmail.com>
2023-12-25 17:53:03 -0300

    add weather repository

---
+++ WeatherAPI/Data/Repositories/WeatherRepository.cs
@@ -0,0 +1,28 @@
+﻿using WeatherAPI.Models;
+
+namespace WeatherAPI.Data.Repositories
+{
+    public class WeatherRepository
+    {
+        private readonly WeatherDbContext _dbContext;
+
+        public WeatherRepository(WeatherDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddWeatherAsync(WeatherResponse weatherData)
+        {
+            var historicData = new WeatherHistoric
+            {
+                City = weatherData.Name,
+                Temperature = weatherData.Main.Temp,
+                Feels_Like = weatherData.Main.Feels_Like,
+                Timestamp = DateTime.UtcNow
+            };
+
+            _dbContext.WeatherHistorics.Add(historicData);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
