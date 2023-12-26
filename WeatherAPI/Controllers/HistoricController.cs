caa4e757304bd934a4578a13030bc3c9e3069943
Gon <gonzaa1694@gmail.com>
2023-12-25 23:29:01 -0300

    add Historic Controller

---
+++ WeatherAPI/Controllers/HistoricController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class HistoricController : ControllerBase
+{
+    private readonly IHistoricService _historicService;
+
+    public HistoricController(IHistoricService historicService)
+    {
+        _historicService = historicService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetHistoricWeather(string city)
+    {
+        try
+        {
+            var historicWeather = await _historicService.GetHistoricWeatherAsync(city);
+            return Ok(historicWeather);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest($"Error: {ex.Message}");
+        }
+    }
+}
