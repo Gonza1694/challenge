64b46e0d690308f79169d2e0cd409338f0b3186c
Gon <gonzaa1694@gmail.com>
2023-12-26 15:13:20 -0300

    add more unit test

--- WeatherAPI.UnitTests/DbTests.cs
+++ WeatherAPI.UnitTests/DbTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 using WeatherAPI.Data;
+using WeatherAPI.Models;
 using WeatherAPI.Utils;
 
 public class DbTests
@@ -25,4 +27,53 @@ public class DbTests
         }
     }
 
+    [Fact]
+    public void EnsureWeatherHistoricTableExists()
+    {
+        var options = new DbContextOptionsBuilder<WeatherDbContext>()
+            .UseSqlServer(ConnectionString.GetSQLString)
+            .Options;
+
+        using (var context = new WeatherDbContext(options))
+        {
+            context.Database.EnsureCreated();
+
+            var tableExists = context.Model.GetEntityTypes()
+                .Any(e => e.ClrType == typeof(WeatherHistoric));
+
+            Assert.True(tableExists, "La tabla WeatherHistoric no existe en el modelo de la base de datos");
+        }
+    }
+
+    [Fact]
+    public void EnsureWeatherHistoricTableHasCorrectColumns()
+    {
+        var options = new DbContextOptionsBuilder<WeatherDbContext>()
+            .UseSqlServer(ConnectionString.GetSQLString)
+            .Options;
+
+        using (var context = new WeatherDbContext(options))
+        {
+            context.Database.EnsureCreated();
+
+            var weatherHistoricEntityType = context.Model.FindEntityType(typeof(WeatherHistoric));
+
+            Assert.NotNull(weatherHistoricEntityType);
+
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.Id));
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.Country));
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.City));
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.Temperature));
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.Feels_Like));
+            AssertPropertyExists(weatherHistoricEntityType, nameof(WeatherHistoric.Timestamp));
+        }
+    }
+
+    private void AssertPropertyExists(IEntityType entityType, string propertyName)
+    {
+        var property = entityType.FindProperty(propertyName);
+        Assert.NotNull(property);
+        Assert.Equal(propertyName, property.Name);
+    }
 }
+
