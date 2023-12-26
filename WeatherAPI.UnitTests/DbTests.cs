37112f76c18f71c964270b35227d24872f291c27
Gon <gonzaa1694@gmail.com>
2023-12-26 14:46:17 -0300

    add unit test

---
+++ WeatherAPI.UnitTests/DbTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAPI.Data;
+using WeatherAPI.Utils;
+
+public class DbTests
+{
+    [Fact]
+    public void EnsureDatabaseCanConnect()
+    {
+        var options = new DbContextOptionsBuilder<WeatherDbContext>()
+            .UseSqlServer(ConnectionString.GetSQLString)
+            .Options;
+
+        using (var context = new WeatherDbContext(options))
+        {
+            context.Database.OpenConnection();
+            try
+            {
+                context.Database.EnsureCreated();
+            }
+            finally
+            {
+                context.Database.CloseConnection();
+            }
+        }
+    }
+
+}
