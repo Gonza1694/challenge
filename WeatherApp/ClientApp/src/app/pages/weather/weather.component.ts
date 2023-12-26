1c328d37f07c0b398884ab91ce7c4c9dba681581
Gon <gonzaa1694@gmail.com>
2023-12-25 23:55:09 -0300

    add logic to get historic and show the spinner

--- WeatherApp/ClientApp/src/app/pages/weather/weather.component.ts
+++ WeatherApp/ClientApp/src/app/pages/weather/weather.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
+import { HistoricService } from '../../services/historic.service';
+import { forkJoin, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
@@ -7,24 +9,41 @@ import { WeatherService } from '../../services/weather.service';
   styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
-  selectedCity: string = "";
+  selectedCity: string = '';
   weatherData: any;
+  showHistoric: boolean = false;
+  cities: string[] = ['San Miguel de Tucumán', 'Yerba Buena'];
+  weatherHistoric: any[] = [];
+  loading: boolean = false;
 
-  cities: string[] = ["San Miguel de Tucumán", "Buenos Aires"];
-
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService, private historicService: HistoricService) { }
 
   ngOnInit() {
+    this.loading = false;
   }
 
   getWeather() {
     if (this.selectedCity) {
-      this.weatherService.getWeather(this.selectedCity).subscribe((data) => {
-        this.weatherData = data;
-        console.log(this.weatherData);
-      });
+      this.loading = true;
+
+      const weatherSubscription = this.weatherService.getWeather(this.selectedCity);
+      const historicSubscription: Observable<any[]> = this.showHistoric
+        ? this.historicService.getHistoricWeather(this.selectedCity)
+        : of([]);
+
+      forkJoin([weatherSubscription, historicSubscription]).subscribe(
+        ([weatherData, weatherHistoric]) => {
+          this.weatherData = weatherData;
+          this.weatherHistoric = weatherHistoric;
+          this.loading = false;
+        },
+        (error) => {
+          console.error('Error:', error);
+          this.loading = false;
+        }
+      );
     } else {
-      alert("Por favor, seleccione una ciudad antes de consultar.");
+      alert('Por favor, seleccione una ciudad antes de consultar.');
     }
   }
 }
